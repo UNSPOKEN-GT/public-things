3c0eb4d0e3adeeb00e2160642d06dbf9d5b1d385
23524TESTING-ACCOUNT <104364978+UNSPOKEN-GT@users.noreply.github.com>
2022-08-21 04:52:38 +0200

    Create simple-vgcrash.lua

---
+++ simple-vgcrash.lua
@@ -0,0 +1,14 @@
+cmd("gear me 94794847")
+
+task.wait(0.36)
+
+game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack.VampireVanquisher)
+
+task.wait(0.155)
+
+for i = 1,10 do
+   cmd("size me .3")
+   cmd("size me .3")
+   cmd("size me .3")
+   cmd("clone me")
+end
