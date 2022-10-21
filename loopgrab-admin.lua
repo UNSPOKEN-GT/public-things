4c4b1d49c7da22b851a56a387f80f71584d2ca47
23524TESTING-ACCOUNT <104364978+UNSPOKEN-GT@users.noreply.github.com>
2022-10-21 17:14:19 +0200

    Update loopgrab-admin.lua

--- loopgrab-admin.lua
+++ loopgrab-admin.lua
@@ -1,19 +1,30 @@
-local PadCheck = true
-while PadCheck == true do
-			wait(0)
-			if not game:GetService("Workspace").Terrain["_Game"].Admin.Pads:FindFirstChild(game.Players.LocalPlayer.Name .. "'s admin") then
-				if game:GetService("Workspace").Terrain["_Game"].Admin.Pads:FindFirstChild("Touch to get admin") then
-					local pad = game:GetService("Workspace").Terrain["_Game"].Admin.Pads:FindFirstChild("Touch to get admin"):FindFirstChild("Head")
-					local padCFrame = game:GetService("Workspace").Terrain["_Game"].Admin.Pads:FindFirstChild("Touch to get admin"):FindFirstChild("Head").CFrame
-					wait(0.125)
-					pad.CanCollide = false
-					repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-					pad.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-					wait(0.125)
-					pad.CFrame = padCFrame
-					pad.CanCollide = true
-				else
-					fireclickdetector(game:GetService("Workspace").Terrain["_Game"].Admin.Regen.ClickDetector, 0)
-				end
-			end
+local check = true
+
+local workspace = game:GetService("Workspace")
+local terrain = workspace.Terrain["_Game"]
+local pads = terrain.Admin.Pads
+
+local lp = game.Players.LocalPlayer
+local char = lp.Character
+
+local function loopgrab()
+    if not pads:FindFirstChild(lp.Name .. "'s admin") then
+        if pads:FindFirstChild("Touch to get admin") then
+               local pad = pads:FindFirstChild("Touch to get admin"):FindFirstChild("Head")
+               local padCFrame = pads:FindFirstChild("Touch to get admin"):FindFirstChild("Head").CFrame
+               wait(.125)
+               pad.CanCollide = false
+               repeat wait() until char:FindFirstChild("HumanoidRootPart")
+	       pad.CFrame = char.HumanoidRootPart.CFrame
+	       wait(.125)
+	       pad.CFrame = padCFrame
+	       pad.CanCollide = true
+	else
+	       fireclickdetector(terrain.Admin.Regen.ClickDetector, 0)
+	end
+    end
+end
+
+while check do wait()
+    loopgrab()
 end
