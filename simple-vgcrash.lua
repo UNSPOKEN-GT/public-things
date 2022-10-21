c92eaf5f0d3c6451baa270820ef0baaa71bd58a1
23524TESTING-ACCOUNT <104364978+UNSPOKEN-GT@users.noreply.github.com>
2022-10-21 17:20:42 +0200

    Update simple-vgcrash.lua

--- simple-vgcrash.lua
+++ simple-vgcrash.lua
@@ -1,14 +1,24 @@
+local players = game.Players
+local lp = players.LocalPlayer
+
+-- CHAT BYPASS >> (credits to i actually forgot lmao)
+local ChatBar = lp.PlayerGui.Chat.Frame.ChatBarParentFrame.Frame.BoxFrame.Frame.ChatBar
+local function cmd(massage)
+    ChatBar:SetTextFromInput(massage)
+    plaers:Chat(massage)
+    ChatBar.Text = ""
+end
+
+-- SCRIPT >>
 cmd("gear me 94794847")
 
-task.wait(0.36)
-
-game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack.VampireVanquisher)
-
-task.wait(0.155)
+task.wait(.36) -- any lower would probably break the script
+lp.Character.Humanoid:EquipTool(lp.Backpack.VampireVanquisher)
+task.wait(.155)-- any lower would probably break the script
 
 for i = 1,10 do
-   cmd("size me .3")
-   cmd("size me .3")
-   cmd("size me .3")
-   cmd("clone me")
+    cmd("size me .3")
+    cmd("size me .3")
+    cmd("size me .3")
+    cmd("clone me")
 end
