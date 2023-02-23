2f7f8d7b3623161b5acd9829a7c40ceb26155731
23524TESTING-ACCOUNT <104364978+UNSPOKEN-GT@users.noreply.github.com>
2023-02-23 16:44:24 +0000

    Create toiltetpaperlagger.lua

---
+++ public bathroom simulator/toiltetpaperlagger.lua
@@ -0,0 +1,18 @@
+-- this script will spam toilet paper, u need a shit ton of tp for it to start lagging
+ 
+local LP = game.Players.LocalPlayer
+game.ReplicatedStorage.ToolEvents.TpRollEvent:FireServer()
+repeat wait() until LP.Character
+for i,v in pairs(LP.Backpack:GetChildren()) do
+   if v.Name == "TpRoll" then
+       v.Parent = LP.Character
+   end
+end
+while true do
+   wait()
+   for i,v in pairs (LP.Character:GetChildren()) do
+       if v.Name == "TpRoll" then
+           v:Activate()
+       end
+   end
+end
